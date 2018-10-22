9ef69d1abfe057dc346923586b56f00540ee4825
Christopher Cowell <chris.cowell@cambiahealth.com>
2018-10-22 09:37:59 -0700

    wip

---
+++ features/sample.feature
@@ -0,0 +1,5 @@
+Feature: all the things
+
+  Scenario: the first scenario
+    When foo
+    Then bar
---
+++ features/step_definitions/sample_steps.rb
@@ -0,0 +1,7 @@
+When("foo") do
+  puts("in When foo")
+end
+
+Then("bar") do
+  puts("in Then bar")
+end
